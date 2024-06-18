44f9aa20765b6e53229e883007205587dc83990f
Wong-Jia-Jun <xxjiajun@gmail.com>
2024-06-18 04:15:59 -0400

    attempt to fix flake8 e302

--- service/routes.py
+++ service/routes.py
@@ -62,8 +62,9 @@ def create_accounts():
 ######################################################################
 
 # ... place you code here to LIST accounts ...
-
 @app.route("/accounts", methods=["GET"])
+
+
 def list_accounts():
     """
     List all Accounts
@@ -81,8 +82,9 @@ def list_accounts():
 ######################################################################
 
 # ... place you code here to READ an account ...
-
 @app.route("/accounts/<int:account_id>", methods=["GET"])
+
+
 def get_accounts(account_id):
     """
     Reads an Account
@@ -99,8 +101,9 @@ def get_accounts(account_id):
 ######################################################################
 
 # ... place you code here to UPDATE an account ...
-
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
+
+
 def update_accounts(account_id):
     """
     Update an Account
@@ -119,8 +122,9 @@ def update_accounts(account_id):
 ######################################################################
 
 # ... place you code here to DELETE an account ...
-
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
+
+
 def delete_accounts(account_id):
     """
     Delete an Account
